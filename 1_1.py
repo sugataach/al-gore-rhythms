225ff3b02a42e0b4a7ea7145f1d7773b0cea64ef
Sugata Acharjya <sugata.a@gmail.com>
2017-07-04 22:56:56 -0400

    add q1.1

---
+++ 1_1.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+
+'''
+Determine if a string has all unique characters
+'''
+
+def str_check(str):
+    if str is None:
+        return False
+    
+    h = defaultdict(int)
+    for char in str:
+        if char in h:
+            return False
+        h[char] += 1
+    return True
+
+print(str_check('bb') == False)
+print(str_check('cat') == True)
+print(str_check('catc') == True)
+
+
