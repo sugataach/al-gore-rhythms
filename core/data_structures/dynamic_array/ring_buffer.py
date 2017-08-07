d0c346f48fdf0118f07446fd25881b1571670d59
Sugata Acharjya <sugata.a@gmail.com>
2017-08-07 14:29:50 -0400

    add ring buffer

---
+++ core/data_structures/dynamic_array/ring_buffer.py
@@ -0,0 +1,23 @@
+"""
+A Ring Buffer is a buffer with a fixed size.
+When it fills up, it overwrites from the beginning.
+"""
+
+class RingBuffer(object):
+    def __init__(self, max_size):
+        self.data = []
+        self.max_size = max_size
+
+    def append(self, el):
+        self.data.append(el)
+        if len(self.data) == self.max_size:
+            self.curr = 0
+            self.__class__ = RingBufferFull
+
+class RingBufferFull(object):
+    def __init__(self):
+        raise 'You should use a RingBuffer'
+
+    def append(self, el):
+        self.data[curr] = el
+        self.curr = (self.data + 1) % self.max_size
