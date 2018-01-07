421459fa3ee1174451952dff702a962266d71795
Sugata Acharjya <sugata.a@gmail.com>
2018-01-06 23:45:43 -0500

    q24

---
+++ arden/test_q24.py
@@ -0,0 +1,19 @@
+'''
+Given a number, find the next higher number using only the digits in the given number. For example if the given number is 1234, next higher number with same digits is 1243.
+
+Approach:
+
+
+Time ->
+
+Space ->
+
+'''
+import pytest
+
+def higher_num_same_digits(num):
+    pass
+
+def test_higher_num_same_digits():
+
+    assert(higher_num_same_digits()) == 1
