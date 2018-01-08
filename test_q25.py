093585f7806923c62117b35e010c9e54e4ca419e
Sugata Acharjya <sugata.a@gmail.com>
2018-01-07 23:25:45 -0500

    q25

---
+++ test_q25.py
@@ -0,0 +1,19 @@
+'''
+Remove duplicate characters in a given string keeping only the first occurrences. For example, if the input is ‘tree traversal’ the output will be ‘tre avsl’.
+
+Approach:
+
+
+Time ->
+
+Space ->
+
+'''
+import pytest
+
+def remove_dup_chars(input_string):
+    pass
+
+def test_higher_num_same_digits():
+
+    assert(remove_dup_chars(input_string)) == 1
